ab6cc6ba3549e249a09af18f12387a5b39382c6e
notroot <notroot@ubuntu.(none)>
2014-04-22 20:10:45 +0000

    Updated checkstyle script

--- tools/java_Analysis/checkstyle/checkStyle.sh
+++ tools/java_Analysis/checkstyle/checkStyle.sh
@@ -12,6 +12,67 @@
 ### Date/Time when the script begins to run
 date1=$(date +"%s")
 
+### Location of all files to be analyzed
+inputDirectory=../testinput/
+
+
+cd checkstyle/
+
+
+### Loop through all files in the input directory
+for i in $(find $inputDirectory -mindepth 1 -type d ) # mindepth ignore the top layer
+do
+	
+	### Begin running checkstyle on each
+	rm -f temp.txt
+	touch temp.txt
+	
+	### messy, but I don't see a way around this
+	
+	#pwd
+	#cd checkstyle
+	#ls $inputDirectory/$i   	
+	#temp="`java -jar checkstyle-5.7-all.jar -c all-checkstyle-checks.xml $target 2>&1 > temp.txt`"
+	### Written to temp file since a variable is too long to parse afterwards
+	#java -jar checkstyle-5.7-all.jar -c all-checkstyle-checks.xml $inputDirectory/$i/* 2>&1 > temp.txt
+	#`java -jar checkstyle-5.7-all.jar -c all-checkstyle-checks.xml $target 2>&1 > temp.txt`
+	
+	
+	#echo Application: $(basename $i)
+	
+	temp="`java -jar checkstyle-5.7-all.jar -c all-checkstyle-checks.xml $inputDirectory/$i/* 2>&1 > temp.txt`"
+
+	fileTarget=temp.txt
+#	cat $fileTarget
+
+	defectCount=`grep -o '.java' $fileTarget | wc -l`
+
+	date2=$(date +"%s")
+	diff=$(($date2-$date1))
+
+	touch temp.txt
+	echo "$(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed." 
+
+	rm -f temp.txt
+
+
+	echo FileName: $(basename $i)
+	echo DefectCount: $defectCount
+
+
+
+	
+done
+
+
+$(basename $i)
+
+echo "$(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed. - Total Time" 
+
+
+exit
+
+
 
 
 
