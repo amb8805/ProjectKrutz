bbd53445aa5335ba68e4fc816f3a0be28bc09204
cklimkowsky <cklimkowsky@gmail.com>
2014-04-25 20:44:01 -0400

    Added from_crawler method to ProxyMiddleware

--- scraper/scraper/middlewares.py
+++ scraper/scraper/middlewares.py
@@ -4,6 +4,10 @@ class ProxyMiddleware(object):
 	def __init__(self, settings):
 		self.proxy_list = settings.get('PROXY_LIST')
 
+	@classmethod
+	def from_crawler(cls, crawler):
+		return cls(crawler.settings)
+
 	def process_request(self, request, spider):
 		proxy = random.choice(proxy_list)
 		request.meta['proxy'] = '%s//%s:%s' % (proxy['type'], proxy['ip'], proxy['port'])
