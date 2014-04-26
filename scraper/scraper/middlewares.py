4571f157855165e273e8579c53d2e1ec63b1cb80
cklimkowsky <cklimkowsky@gmail.com>
2014-04-25 20:21:46 -0400

    Fixed indentation in middlewares.py

--- scraper/scraper/middlewares.py
+++ scraper/scraper/middlewares.py
@@ -2,8 +2,8 @@ import random
 
 class ProxyMiddleware(object):
 	def __init__(self, settings):
-        self.proxy_list = settings.get('PROXY_LIST')
+		self.proxy_list = settings.get('PROXY_LIST')
 
-    def process_request(self, request, spider):
-        proxy = random.choice(proxy_list)
-        request.meta['proxy'] = '%s//%s:%s' % (proxy['type'], proxy['ip'], proxy['port'])
+	def process_request(self, request, spider):
+		proxy = random.choice(proxy_list)
+		request.meta['proxy'] = '%s//%s:%s' % (proxy['type'], proxy['ip'], proxy['port'])
