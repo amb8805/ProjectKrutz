0480da9571b9a2db17cae867fe2c5b8bf6a06551
cklimkowsky <cklimkowsky@gmail.com>
2014-04-28 17:30:17 -0400

    Added some more (hopefully) working proxies

--- scraper/scraper/middlewares.py
+++ scraper/scraper/middlewares.py
@@ -10,4 +10,11 @@ class ProxyMiddleware(object):
 
 	def process_request(self, request, spider):
 		proxy = random.choice(self.proxy_list)
+
+		log.msg('Downloading using <%s>' % proxy, level=log.INFO)
+
 		request.meta['proxy'] = '%s://%s:%s' % (proxy['type'], proxy['ip'], proxy['port'])
+
+	def process_exception(self, request, exception, spider):
+		proxy = request.meta['proxy']
+		log.msg('Failed proxy <%s>' % (proxy, len(self.proxy_list)), level=log.WARNING)
--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -4,13 +4,13 @@ SPIDER_MODULES = ['scraper.spiders']
 NEWSPIDER_MODULE = 'scraper.spiders'
 
 ITEM_PIPELINES = {
-	'scraper.pipelines.UniqueIdentifierPipeline' : 100,
-	'scraper.pipelines.SQLiteStorePipeline' : 300,
-	'scraper.pipelines.APKFilesPipeline' : 800,
+	# 'scraper.pipelines.UniqueIdentifierPipeline' : 100,
+	# 'scraper.pipelines.SQLiteStorePipeline' : 300,
+	# 'scraper.pipelines.APKFilesPipeline' : 800,
 }
 
 DOWNLOADER_MIDDLEWARES = {
-	'scraper.middlewares.ProxyMiddleware' : 100,
+	# 'scraper.middlewares.ProxyMiddleware' : 100,
 }
 
 FILES_STORE = './downloads'
@@ -28,6 +28,41 @@ PROXY_LIST = [
 	{
 		'type'	: 'https',
 		'ip'	: '203.172.248.70',
+		'port'	: '3128'
+	},
+	{
+		'type'	: 'https',
+		'ip'	: '88.255.147.83',
+		'port'	: '8080'
+	},
+	{
+		'type'	: 'http',
+		'ip'	: '220.248.180.149',
+		'port'	: '3128'
+	},
+	{
+		'type'	: 'https',
+		'ip'	: '202.107.231.153',
+		'port'	: '8088'
+	},
+	{
+		'type'	: 'http',
+		'ip'	: '189.208.57.239',
+		'port'	: '8080'
+	},
+	{
+		'type'	: 'http',
+		'ip'	: '195.103.219.108',
+		'port'	: '8080'
+	},
+	{
+		'type'	: 'http',
+		'ip'	: '218.28.96.39',
+		'port'	: '3128'
+	},
+	{
+		'type'	: 'https',
+		'ip'	: '222.124.149.178',
 		'port'	: '3128'
 	}
 ]
