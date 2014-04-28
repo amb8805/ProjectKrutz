3cde018262b209e88a6324514980bf1d5767e7bb
Daniel Krutz <dxkvse@vm-007.casci.rit.edu>
2014-04-28 18:31:35 -0400

    Added permissions for sqlite DB

--- updatePermissions.sh
+++ updatePermissions.sh
@@ -19,5 +19,6 @@ chmod -R a+x tools/java_Analysis/checkstyle/checkStyle.sh
 chmod -R a+x tools/java_Analysis/CloneDetection/nicad3
 chmod -R a+x tools/androguard/androrisk.py
 chmod -R a+x tools/java_Analysis/CloneDetection/scripts/NiCadPair-Ubuntu
+chmod -R a+x Evolution\ of\ Android\ Applications.sqlite
 
 
