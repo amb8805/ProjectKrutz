8fa408ea4e600572cb28777265421445565f30fb
cklimkowsky <cklimkowsky@gmail.com>
2014-04-28 22:58:48 -0400

    Removed process_exception function from middlewares.py

--- scraper/scraper/middlewares.py
+++ scraper/scraper/middlewares.py
@@ -14,7 +14,3 @@ class ProxyMiddleware(object):
 		log.msg('Downloading using <%s>' % proxy, level=log.INFO)
 
 		request.meta['proxy'] = '%s://%s:%s' % (proxy['type'], proxy['ip'], proxy['port'])
-
-	def process_exception(self, request, exception, spider):
-		proxy = request.meta['proxy']
-		log.msg('Failed proxy <%s>' % (proxy, len(self.proxy_list)), level=log.WARNING)
