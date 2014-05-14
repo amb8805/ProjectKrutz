2be029dbea06704a40a900a65cac670379ac136d
Casey Klimkowsky <cek3403@vm-007.casci.rit.edu>
2014-05-14 18:21:41 -0400

    Finished writing database cleanup script, and cleaned up the database

--- cleanDatabase.sh
+++ cleanDatabase.sh
@@ -1,3 +1,15 @@
-sqlite3 Evolution\ of\ Android\ Applications.sqlite "SELECT ApkId FROM ApkInformation" | while read id; do
-	echo "$id"
+array=()
+echo "Begin cleaning up the database"
+while read id; do
+	file="$id.apk"
+	if [ ! -f "scraper/downloads/full/$file" ]; then
+		echo "File $file does not exist"
+		array[${#array[*]}]=$id
+	fi
+done < <(sqlite3 Evolution\ of\ Android\ Applications.sqlite "SELECT ApkId FROM ApkInformation")
+
+for apk in ${array[@]}; do
+	echo "Removing record $apk from database"
+	sqlite3 Evolution\ of\ Android\ Applications.sqlite "DELETE FROM ApkInformation WHERE ApkId = '$apk'"
 done
+echo "Database cleanup complete"
