1f4eff2fdf7a63c5661bf5d1d8b2ef3f8801102f
cklimkowsky <cklimkowsky@gmail.com>
2014-05-15 16:58:12 -0400

    Added IsDownloaded column to database, and updated cleanDatabase.sh

--- cleanDatabase.sh
+++ cleanDatabase.sh
@@ -9,20 +9,23 @@ while read id; do
 done < <(sqlite3 EvolutionOfAndroidApplications.sqlite "SELECT ApkId FROM ApkInformation")
 
 for apk in ${array[@]}; do
-	echo "Removing record $apk from database"
-	sqlite3 EvolutionOfAndroidApplications.sqlite "DELETE FROM ApkInformation WHERE ApkId = '$apk'"
+	# echo "Removing record $apk from database"
+	# sqlite3 EvolutionOfAndroidApplications.sqlite "DELETE FROM ApkInformation WHERE ApkId='$apk'"
+
+	echo "Setting isDownloaded flag for record $apk"
+	sqlite3 EvolutionOfAndroidApplications.sqlite "UPDATE ApkInformation SET IsDownloaded=0 WHERE ApkId='$apk'"
 done
 echo "Database cleanup complete"
 
-echo "Begin deleting files that don't have a database record"
-for file in scraper/downloads/full/*.apk; do
-	name=${file##*/}
-	base=${name%.apk}
+# echo "Begin deleting files that don't have a database record"
+# for file in scraper/downloads/full/*.apk; do
+# 	name=${file##*/}
+# 	base=${name%.apk}
 	
-	apk=`sqlite3 EvolutionOfAndroidApplications.sqlite "SELECT Name FROM ApkInformation WHERE ApkId='$base'"`
-	if [ -z "${apk}" ]; then
-		echo "Deleting file $file"
-		rm -f $file
-	fi
-done
-echo "Finished deleting files"
+# 	apk=`sqlite3 EvolutionOfAndroidApplications.sqlite "SELECT Name FROM ApkInformation WHERE ApkId='$base'"`
+# 	if [ -z "${apk}" ]; then
+# 		echo "Deleting file $file"
+# 		rm -f $file
+# 	fi
+# done
+# echo "Finished deleting files"
