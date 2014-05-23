c8259c8ce1533f36495da5c0d1cf89aa997de3f1
Daniel Krutz <dxkvse@vm-007.casci.rit.edu>
2014-05-23 10:16:50 -0400

    update permissions script

--- updatePermissions.sh
+++ updatePermissions.sh
@@ -13,6 +13,7 @@ echo "Update Permissions"
 chmod -R a+x toolsScript.sh
 chmod -R a+x tools/java_Analysis/APK_to_JAVA/convert_APK_Java.sh
 chmod -R a+x tools/java_Analysis/APK_to_JAVA/dex2jar-0.0.9.15/dex2jar.sh
+chmod -R a+x tools/java_Analysis/jlint/run_jlint.sh
 chmod -R a+x tools/java_Analysis/CloneDetection/runclones.sh
 chmod -R a+x tools/java_Analysis/checkstyle/checkStyle.sh
 
