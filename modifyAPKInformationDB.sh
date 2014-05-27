58fe3ce986637ffee7630e930bbe4388d5c80c93
dan7800@yahoo.com <dan7800@yahoo.com>
2014-05-27 18:05:26 +0000

    Script not only gets the file size

--- modifyAPKInformationDB.sh
+++ modifyAPKInformationDB.sh
@@ -4,23 +4,24 @@
 ###			This will ultimately be refactored and placed into 
 
 
-#### Convert the date format to one which we can sort
-convert_date () {
+		#### Convert the date format to one which we can sort
+		#convert_date () {
 
-	day=${2//,/""}
-    local months=( January February March April May June July August September October November December )
-    local i
-    for (( i=0; i<11; i++ )); do
-        [[ $1 = ${months[$i]} ]] && break
-    done
-    #printf "%4d%02d%02d\n" $3 $(( i+1 )) $2
-    printf "%4d%02d%02d\n" $3 $(( i+1 )) $day
-}
+		#	day=${2//,/""}
+		#   local months=( January February March April May June July August September October November December )
+		#    local i
+		#    for (( i=0; i<11; i++ )); do
+		#        [[ $1 = ${months[$i]} ]] && break
+		#    done
+		#    #printf "%4d%02d%02d\n" $3 $(( i+1 )) $2
+		#    printf "%4d%02d%02d\n" $3 $(( i+1 )) $day
+		#}
 
 
 #### Directory of APK files to be analyzed
 apkInputDir=scraper/downloads/full
 
+### Name of the sqlite file being analyzed
 dbname=EvolutionOfAndroidApplications.sqlite
 
 
@@ -28,21 +29,6 @@ date1=$(date +"%s") ## Start date of the script
 logLocation=logs/modifyAPKInfo.log
 touch $logLocation
 echo "ModifyAPKInfo Start:" `date` >> $logLocation
-
-
-#### Loop through all of the records in the database
-#### Done like this since not all of the files are actual APK Files
-
-
-
-
-	sqlite3 $dbname  "UPDATE ApkInformation SET GeneratedFileSize=$fileSize WHERE rowid=$rowid;"	
-
-
-
-exit
-
-
 
 
 #### Loop through all of the apk files in the directory
@@ -55,14 +41,6 @@ FILES=$(find $apkInputDir -type f -name '*.apk')
 	#echo $apkID
 	rowid=`sqlite3 $dbname "SELECT rowid FROM ApkInformation WHERE ApkId='$apkID';"`
 
-	### Modify the minimum operating system
-	# If the minimum OS is 1.3 and up, the column "ModifiedOS" should read 1.3
-	minos=`sqlite3 $dbname "SELECT OperatingSystems FROM ApkInformation WHERE rowid='$rowid';"`
-	minos=${minos//and up/""} ### Remove the and up	
-	sqlite3 $dbname  "UPDATE ApkInformation SET ModifiedOS=\"$minos\" WHERE rowid=$rowid;"	
-
-	### Get the size of the apk file & Update the "GeneratedFileSize column" 
-	## Analyze the examined apk file and get its file size. This file size should then be recorded and put into the DB.
 
 	### Get the size of the apk file. The extra command has to be used to remove the filename
 	#set -- $(du $f) ## Old version, only reported ~1/2 the size of the file
@@ -70,24 +48,39 @@ FILES=$(find $apkInputDir -type f -name '*.apk')
 	fileSize=$1
 	sqlite3 $dbname  "UPDATE ApkInformation SET GeneratedFileSize=$fileSize WHERE rowid=$rowid;"	
 
+	### The rest is commented out since not all of the information being pulled will have apk files
+
+
+	### Modify the minimum operating system
+	# If the minimum OS is 1.3 and up, the column "ModifiedOS" should read 1.3
+	#	minos=`sqlite3 $dbname "SELECT OperatingSystems FROM ApkInformation WHERE rowid='$rowid';"`
+	#	minos=${minos//and up/""} ### Remove the and up	
+	#	sqlite3 $dbname  "UPDATE ApkInformation SET ModifiedOS=\"$minos\" WHERE rowid=$rowid;"	
+
+	### Get the size of the apk file & Update the "GeneratedFileSize column" 
+	## Analyze the examined apk file and get its file size. This file size should then be recorded and put into the DB.
+
+	
 	#### Generate the modified PublicationDate
 	# Right now the DatePublishedDate is April 5, 2014
 	# This should be saved in the "ModifiedDatePublished" column as 4/5/2014
-	DatePublished=`sqlite3 $dbname "SELECT DatePublished FROM ApkInformation WHERE rowid='$rowid';"`
-	
-	### Convert to modified date published
-	d=$( convert_date $DatePublished )
-	#d=$( convert_date "March 17, 2014" )
-	
-	sqlite3 $dbname  "UPDATE ApkInformation SET modifiedDatePublished=$d WHERE rowid=$rowid;"
+	#	DatePublished=`sqlite3 $dbname "SELECT DatePublished FROM ApkInformation WHERE rowid='$rowid';"`
+		
+		### Convert to modified date published
+	#	d=$( convert_date $DatePublished )
+		#d=$( convert_date "March 17, 2014" )
+		
+	#	sqlite3 $dbname  "UPDATE ApkInformation SET modifiedDatePublished=$d WHERE rowid=$rowid;"
 
 done
+
+
+#### Put everything into logs
+# 
 
 date2=$(date +"%s")
 diff=$(($date2-$date1))
 echo "End:" `date` >> $logLocation
 echo "Total Time $(($diff / 60)) minutes and $(($diff % 60)) seconds."  >> $logLocation
 
-#### Put everything into logs
-# 
 
