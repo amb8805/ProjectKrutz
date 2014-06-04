27a6428ea750dba4c508f290c9231397435b9431
dan7800@yahoo.com <Dan>
2014-06-04 04:51:03 +0000

    Working version of simcad ?

--- tools/java_Analysis/CloneDetection/SimCad-2.2/run_simcad.sh
+++ tools/java_Analysis/CloneDetection/SimCad-2.2/run_simcad.sh
@@ -6,6 +6,22 @@
 	#inputLocation=$1
 	inputLocation=../../javaOutput
 
+
+	logLocation=logs/Simcad.log
+
+	### Remove the log if it is there
+	rm -f $logLocation
+
+	## Create the log
+	touch $logLocation
+
+	echo "Simcad Start:" `date` >> $logLocation
+
+	date1=$(date +"%s")
+
+
+
+
 	### Make the temp output location
 	#tempOutputLocation=tools/java_Analysis/CloneDetection/Simcad-2.2/output/
 	tempOutputLocation=output/
@@ -25,13 +41,13 @@
 
 	for i in $(find $inputLocation -mindepth 1 -maxdepth 1 -type d ) 
 	do
-	  
+	  	echo "Simcad Analyzing:" `echo $i` `date` >> ../../../../$logLocation
 		APKFile=$(basename $i)
 		APKFile=${APKFile//%apk/""} ### Remove the apk exension from the apkID
 		mkdir -p $tempOutputLocation/$APKFile # Create the temp output location
 		
 		### Examine each by simcad
-#		temp=`./simcad2 -s $i -l java -o $tempOutputLocation/$APKFile`
+		temp=`./simcad2 -s $i -l java -o $tempOutputLocation/$APKFile`
 	
 
 		### If an exception is thrown, then skip the rest
@@ -41,38 +57,39 @@
 		else
 			echo blah
 			### Parse the expected output
-#			sourceFragmentCount=`sed 's/^.*Total source fragment ://; s/Total clone fragment.*$//' <<< $temp`
-#			cloneFragmentCount=`sed 's/^.*Total clone fragment ://; s/Total clone group.*$//' <<< $temp`
-#			cloneGroupCount=`sed 's/^.*Total clone group\\/pair ://; s/Pre-Processing time :.*$//' <<< $temp`
+			sourceFragmentCount=`sed 's/^.*Total source fragment ://; s/Total clone fragment.*$//' <<< $temp`
+			cloneFragmentCount=`sed 's/^.*Total clone fragment ://; s/Total clone group.*$//' <<< $temp`
+			cloneGroupCount=`sed 's/^.*Total clone group\\/pair ://; s/Pre-Processing time :.*$//' <<< $temp`
 		fi
 	
+		#echo "APKInformation: " $APKFile
+		#echo Source Fragment: $sourceFragmentCount
+		#echo Clone Fragment: $cloneFragmentCount
+		#echo Clone Group: $cloneGroupCount
 
-		echo "APKInformation: " $APKFile
-		echo Source Fragment: $sourceFragmentCount
-		echo Clone Fragment: $cloneFragmentCount
-		echo Clone Group: $cloneGroupCount
 
-	
+		echo ".......Source Fragment:" `echo $sourceFragmentCount` `date` >> ../../../../$logLocation
+		echo ".......Clone Fragment:" `echo $cloneFragmentCount` `date` >> ../../../../$logLocation
+		echo ".......Clone Group:" `echo $cloneGroupCount` `date` >> ../../../../$logLocation
+
+
 		### Get the row ID
 		rowid=`sqlite3 ../../../../EvolutionOfAndroidApplications.sqlite  "SELECT rowid FROM ApkInformation WHERE ApkId='$APKFile';"`
-		echo $rowid
 
+		### Check to see if the APKID exists in tool results  		
+		APKToolResultsCount=`sqlite3 ../../../../EvolutionOfAndroidApplications.sqlite  "SELECT count(*) FROM ToolResults WHERE rowid='$rowid';"`
 
-		### Inser the information into the database
-
-	
-		### Check to make sure the rowID exist in the toolresults table?
-
-
-
-
-		### Print out message for what each one is doing
-		### Log the amount of time that each action took
-
+		if [[ $APKToolResultsCount -eq 0 ]]; then
+			echo "Insert"
+			sqlite3 ../../../../EvolutionOfAndroidApplications.sqlite  "INSERT INTO ToolResults (ApkId,Simcad_SourceFragment, Simcad_CloneFragment, Simcad_CloneGroup) VALUES ($rowid,$sourceFragmentCount, $cloneFragmentCount, $cloneGroupCount);"
+  		else
+      		echo updated
+      		sqlite3 ../../../../EvolutionOfAndroidApplications.sqlite  "UPDATE ToolResults SET Simcad_SourceFragment=$sourceFragmentCount,  Simcad_CloneFragment=$cloneFragmentCount, Simcad_CloneGroup=$cloneGroupCount WHERE ApkId=$rowid;"
+    	fi
 
 		## Remove the temporary output
 		rm -rf $tempOutputLocation/$APKFile 
-
+	echo "Simcad End Analyzing:" `echo $i` `date` >> ../../../../$logLocation
 	done
 
 	cd ../../../../
@@ -81,11 +98,15 @@
 	### Make sure all output files have been removed
 	rm -rf $tempOutputLocation
 
-	### Loop through all folders in Java Analysis
+
+	echo "End Simcad:" `date` >> $logLocation
+
+	date2=$(date +"%s")
+	diff=$(($date2-$date1))
 
 
-	### Run the application on each folder
-
+	echo "Simcad Total Running Time $(($diff / 60)) minutes and $(($diff % 60)) seconds."  >> $logLocation
+	echo "Simcad End:" `date` >> $logLocation
 
 
 
