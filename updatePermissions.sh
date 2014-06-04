93043799b8a1e68f2e9de81c59214b883aa15254
dan <dan7800@yahoo.com>
2014-06-04 10:43:04 -0400

    Update permissions

--- updatePermissions.sh
+++ updatePermissions.sh
@@ -10,8 +10,8 @@
 
 echo "Update Permissions"
 
-chmod -R a+x tools/java_Analysis/jlint/jlint 		# Not sure if this is needed
-chmod -R a+x tools/java_Analysis/jlint/run_jlint.sh # Not sure if this is needed
+#chmod -R a+x tools/java_Analysis/jlint/jlint 		# Not sure if this is needed
+#chmod -R a+x tools/java_Analysis/jlint/run_jlint.sh # Not sure if this is needed
 
 chmod -R a+x toolsScript.sh
 chmod -R a+x tools/java_Analysis/APK_to_JAVA/convert_APK_Java.sh
@@ -23,6 +23,6 @@ chmod -R a+x tools/java_Analysis/CloneDetection/nicad3
 chmod -R a+x tools/androguard/androrisk.py
 chmod -R a+x tools/java_Analysis/CloneDetection/scripts/NiCadPair-Ubuntu
 chmod -R a+x EvolutionOfAndroidApplications.sqlite
-
+chmod -R a+x tools/java_Analysis/CloneDetection/SimCad-2.2/run_simcad.sh
 
 
