2dfb639b710fc3c87d60e024a02ff96406f51873
Casey Klimkowsky <cek3403@vm-007.casci.rit.edu>
2014-09-13 11:31:51 -0400

    Committing toolScript.sh

--- toolScript.sh
+++ toolScript.sh
@@ -19,19 +19,18 @@
 	rm -rf logs/AndroRiskOutput/
 
 
-
 	echo "Start Stowaway Analysis:" `date` >> $logLocation
-	./tools/stowaway/run_stowaway.sh $APK_Input_Path
+#	./tools/stowaway/run_stowaway.sh $APK_Input_Path
 
 	echo "Start Androguard:" `date` >> $logLocation
-	./androguard.sh $APK_Input_Path
+#	./androguard.sh $APK_Input_Path
 
 	echo "Start java Analysis:" `date` >> $logLocation
-	./tools/java_Analysis/RunAll.sh $APK_Input_Path
+#	./tools/java_Analysis/RunAll.sh $APK_Input_Path
 
 	## Run the script to modify the apkInformation 
 	echo "Start ModifyAPKInformation:" `date` >> $logLocation
-	./modifyAPKInformationDB.sh $APK_Input_Path
+#	./modifyAPKInformationDB.sh $APK_Input_Path
 
 
 	### Gather apk information
