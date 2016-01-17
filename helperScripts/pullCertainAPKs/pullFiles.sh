d02dae2b397eb718df660497c34d222da1a8db1f
dkrutz <dan@go.com>
2016-01-17 12:42:58 -0500

    pushing to prod

--- helperScripts/pullCertainAPKs/pullFiles.sh
+++ helperScripts/pullCertainAPKs/pullFiles.sh
@@ -7,6 +7,7 @@
 	##		Log what was pulled	
 	##		
 
+	clear;
 
 	outputdir=output
 	mkdir -p $outputdir
@@ -15,29 +16,40 @@
 	logLocation=logs/pullFiles.log
 	touch $logLocation
 
+	foundLog=logs/found.log
+	touch $foundLog
+
+	NotfoundLog=logs/Notfound.log
+	touch $NotfoundLog
+
 	inputDir=testtarget
 
-	inputlist=pullList.txt
+#	inputlist=pullList.txt
+	inputlist=combined.lst
+
+
+	# Clear the contents of the log files
+
+
 
 	### -- Debugging only
 	## Clear out the output dir and log files
-		rm -rf $outputdir/*
+	rm -rf $outputdir/*
 
 	### -- End Debugging only
 
-
-
-
 	### Find the file in the input dir. Will need to scan many subdirectories
-
-
-
 
 
 	### Now loop through the input file
 	while read p; do
   		#echo $p
   		### Find each item in the list, and copy it to the output dir
+  		fileFound=false
+
+
+  		echo "Looking for: " $p
+
 
   		FILES=$(find $inputDir -type f -name $p'.apk')
 		for f in $FILES
@@ -46,7 +58,8 @@
 			## Once the file is found, copy it over
 			#echo $f
 			echo "copy " $f
-			cp $f $outputdir
+# Lets wait on copying the file
+#			cp $f $outputdir
 
 			## Write it to the log
 			echo "copy " $f `date` >> $logLocation
@@ -56,16 +69,28 @@
 
 			#cp $f $destinationDir
 			#echo "**** $COUNTER)Copy File: $f" `date` >> $logFile
-			
-	done
+
+			## Log the file as being found
+			fileFound=true
+			echo $f >> $foundLog
 
 
+		done
 
+		## Check to log that the file was not found
+		#if [[ $fileFound eq false ]]; then
+		if [ $fileFound == "false" ]; then
+			echo $p >> $NotfoundLog	
+		#else
+		#	echo $p "true"
+		fi
 
 	done <$inputlist
 
 	### Put a seperator into the log
 	echo " ----------------- Done ----------- " $f `date` >> $logLocation
+	echo " ----------------- Done ----------- " `date` >> $NotfoundLog
+	echo " ----------------- Done ----------- " `date` >> $foundLog
 	### Doesnt work echo " " >> $logLocation ## blank space
 
 
