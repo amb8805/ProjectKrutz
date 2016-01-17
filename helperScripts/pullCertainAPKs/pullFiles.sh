134588282f8735cbc3a5d179fba628930c8e0a7b
dkrutz <dan@go.com>
2016-01-17 13:44:41 -0500

    some simple file cleanup

--- helperScripts/pullCertainAPKs/pullFiles.sh
+++ helperScripts/pullCertainAPKs/pullFiles.sh
@@ -4,11 +4,10 @@
 	##		The intention is that these files would be scanned later on.
 
 	### Todo:
-	##		Log what was pulled	
+	##		Clear the contents of the log file
 	##		
 
 	clear;
-
 
 	outputdir=output
 	mkdir -p $outputdir
@@ -26,13 +25,12 @@
 	#inputDir=testtarget
 	inputDir=/scratch/alreadyscanned
 
-
 #	inputlist=pullList.txt
 	inputlist=combined.lst
 
 
 	# Clear the contents of the log files
-
+	### Todo
 
 
 	### -- Debugging only
@@ -41,18 +39,13 @@
 
 	### -- End Debugging only
 
-	### Find the file in the input dir. Will need to scan many subdirectories
-
-
 	### Now loop through the input file
 	while read p; do
   		#echo $p
   		### Find each item in the list, and copy it to the output dir
-  		fileFound=false
-
+  		fileFound=false ### Mark the file as not being found by default
 
   		echo "Looking for: " $p
-
 
   		FILES=$(find $inputDir -type f -name $p'.apk')
 		for f in $FILES
@@ -61,40 +54,31 @@
 			## Once the file is found, copy it over
 			#echo $f
 			echo "copy " $f
-# Lets wait on copying the file
+			# Actually copy the file
 			cp $f $outputdir
 
 			## Write it to the log
 			echo "copy " $f `date` >> $logLocation
 
-			#fileName=$(basename $f)
-			#let COUNTER+=1
-
-			#cp $f $destinationDir
-			#echo "**** $COUNTER)Copy File: $f" `date` >> $logFile
-
-			## Log the file as being found
-			fileFound=true
-			echo $f >> $foundLog
-
+			
+			fileFound=true ## Log the file as being found
+			echo $f >> $foundLog 
 
 		done
 
 		## Check to log that the file was not found
-		#if [[ $fileFound eq false ]]; then
 		if [ $fileFound == "false" ]; then
-			echo $p >> $NotfoundLog	
+			echo $p >> $NotfoundLog	 ### Write the file to the log as not being found
 		#else
 		#	echo $p "true"
 		fi
 
 	done <$inputlist
 
-	### Put a seperator into the log
+	### Clean up the output of the log files
 	echo " ----------------- Done ----------- " $f `date` >> $logLocation
 	echo " ----------------- Done ----------- " `date` >> $NotfoundLog
 	echo " ----------------- Done ----------- " `date` >> $foundLog
-	### Doesnt work echo " " >> $logLocation ## blank space
 
 
 
